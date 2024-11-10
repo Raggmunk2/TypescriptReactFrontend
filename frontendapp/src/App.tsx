846251c112413df46d196d86ef19d5873a6c1525
Raggmunk2 <linnborgstrom93@gmail.com>
2024-11-10 12:31:18 +0100

    added modal for the onclick of the div to show more info

--- frontendapp/src/App.tsx
+++ frontendapp/src/App.tsx
@@ -3,17 +3,25 @@ import './App.css';
 import Product from './Item';
 
 function App() {
+    // Setting the type for a product
     type ProductType = {
         id: number;
-        title: string;
+        name: string;
         price: number;
         description: string;
         category: string;
         image: string;
       
     }
+    // UseStates for product and useRef for inputRef
     const [products, setProducts] = useState([])
     const inputRef = React.useRef<HTMLInputElement>(null);
+
+    // UseEffect to fetch the data from the backend
+    useEffect(() => {
+        fetchData()
+    }, [])
+    // Function to fetch data from backed API & sets the products to be rendered
     const fetchData = async () => {
         try {  
             const response = await fetch("http://localhost:3000/products", {
@@ -36,13 +44,10 @@ function App() {
         }
     }
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-    const handleSubmit =  async (event: React.FormEvent<HTMLFormElement>) => {
+    const handleChange =  async (event: React.ChangeEvent<HTMLInputElement>) => {
         event.preventDefault();
         if (inputRef.current !== null) {
-            const response = await fetch(`http://localhost:3000/search-products?search=${inputRef.current.value}`, {
+            const response = await fetch(`http://localhost:3000/search?search=${inputRef.current.value}`, {
                 method: "GET",
                 mode: "cors",
                 headers: {
@@ -63,27 +68,22 @@ function App() {
     }
   return (
     <div className='flex flex-col justify-center items-center'>
+        {/* Search bar using tailwind css */}
         <div className="px-4 py-4 flex flex-col justify-center items-center">
-            <div className="px-4 py-4 flex flex-col justify-center items-center">
-                <form onSubmit={handleSubmit} className="px-4 py-4 flex flex justify-center items-center">
-                    
-                    <div className="border border-blue-500 bg-blue-200 rounded-full p-1 pl-2 pr-2 w-96 flex justify-between">
-                        <input type="text" placeholder="Search..." ref={inputRef} className="bg-blue-200 rounded-full p-1 pl-5 pr-5 w-80" />
-                        <button className="text-blue-200 bg-blue-200 mr-2">
-                            <i className="fa-solid fa-magnifying-glass text-md text-blue-500"></i>
-                        </button>
-                    </div>
-
-                        
-                </form>
+            <div className="border border-blue-500 bg-blue-200 rounded-full p-1 pl-2 pr-2 w-96 flex justify-between">
+                <input type="text" placeholder="Search..." ref={inputRef} className="bg-blue-200 rounded-full p-1 pl-5 pr-5 w-80" onChange={handleChange}/>
+                <button className="text-blue-200 bg-blue-200 mr-2">
+                    <i className="fa-solid fa-magnifying-glass text-md text-blue-500"></i>
+                </button>
             </div>
-            
+
         </div>
+        {/* Divs and rendering of the products using tailwind css grid */}
         <div className="px-4 py-4 flex flex-col justify-center items-center">
         <div className="m-1 p-10 grid grid-rows-5 grid-flow-col gap-6">
             
             {products.map((p: ProductType) => (
-                <Product key={p.id} id={p.id} title={p.title} price={p.price} description={p.description} category={p.category }image={p.image} />
+                <Product key={p.id} id={p.id} name={p.name} price={p.price} description={p.description} category={p.category }image={p.image} />
                 
             ))} 
             
--- frontendapp/src/Item.tsx
+++ frontendapp/src/Item.tsx
@@ -1,44 +1,83 @@
-import {useState} from "react";
+import { ComponentType, useState } from "react";
+import Modal from "react-modal";
 
+// Interface for the product props
 interface ProductProps {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+Modal.setAppElement('#root');
+const Product = ({
+  id,
+  name,
+  price,
+  description,
+  category,
+  image,
+}: ProductProps) => {
   
-}
-const Product = ({id,title, price,description,category,image}: ProductProps) => {
-    const [showDesc, setShowDesc] = useState<number[]>([]);
 
-const toggleDesc = (id:number) => {
-    setShowDesc((prevShowDesc) => 
-        prevShowDesc.includes(id) ?
-        prevShowDesc.filter((productId) => productId !== id) :
-        [...prevShowDesc, id]
-    
-    );
-}
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return (
-        <>
-        <div key={id} className="grid grid-flow-row-dense grid-col-1 grid-row-3 border border-stone-300 rounded-md" onClick={() => toggleDesc(id)}>
-                    <div className="row-span-6 flex flex-col justify-end items-center">
-                        <img src={image} className="scale-75" alt="product"/>
-                    </div>
-                    <div className="row-span-6 flex flex-col justify-end items-center pl-4 pr-4 pb-4">
-                    
-                    <label className="text-base font-bold text-center">{title}</label>
-                    <label className="text-base font-bold text-center">{category}</label>
-                    <label className="">Price {price} $</label>
-                    { showDesc.includes(id) && <p>{description}</p> }
-                    </div>
+  // Open the modal
+  const openModal = () => {
+      setIsModalOpen(!isModalOpen);
+      
+  };
+
+  // Close the modal
+  const closeModal = () => {
+      setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div
+      key={id}
+      className="grid grid-flow-row-dense grid-col-1 grid-row-3 border border-stone-300 rounded-md w-64"
+      onClick={() => openModal()}
+    >
+      <div className="row-span-6 flex flex-col justify-end items-center">
+        <img src={image} className="scale-75" alt="product" />
+      </div>
+      <div className="row-span-6 flex flex-col justify-end items-center pl-4 pr-4 pb-4">
+        <label className="text-base font-bold text-center">{name}</label>
+        <label className="text-base font-bold text-center">{category}</label>
+        <label className="">Price {price} $</label>
+      </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        contentLabel="Product Details"
+        onRequestClose={closeModal}
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+        className="bg-white rounded-lg w-96 h-[700px] flex flex-col justify-center px-4"
+        ariaHideApp={false}
+      >
+        <div
+          key={id}
+          className=""
+          onClick={() => openModal}
+        >
+            <div className="flex flex-col justify-center items-center">
+                <img src={image} className="h-64" alt="product" />
                 
-                </div>
-       
-        </>
-    )
-}
+                <div className="flex flex-col justify-end items-center">
+                    <label className="text-base font-bold text-center">{name}</label>
+                    <label className="text-base text-center">{category}</label>
+                    <label className=""><b>Price:</b> {price} $</label>
 
-export default Product
+                    <label className="text-center text-sm">{description}</label>
+                <button className="bg-red-200 px-4 py-2 rounded-full m-2" onClick={closeModal}>Close</button>
+                </div>
+            </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default Product;
